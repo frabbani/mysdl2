55f92369f3d0b671fbd268c310456745b1544667
Faisal Rabbani <f.rabbani@gmail.com>
2024-09-28 23:10:16 -0500

    sdl2 image support

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@
 
 echo "compiling..."
 g++ -std=c++17 -O0 -g3 -Wall -c -fmessage-length=0 -o mysdl2.o "mysdl2.cpp" 
-g++ -shared -o libmysdl2.dll mysdl2.o -lSDL2main -lSDL2 
+g++ -shared -o libmysdl2.dll mysdl2.o -lSDL2main -lSDL2 -lSDL2_image
 echo "done!"
